7aab17aa1a8c12db102a5002abc08bff49c64dfb
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2021-04-12 13:32:39 +0200

    ENH: Implement DTI fit and prediction in parallel with asyncio

--- eddymotion/model.py
+++ eddymotion/model.py
@@ -1,7 +1,13 @@
 """A factory class that adapts DIPY's dMRI models."""
 import warnings
+import concurrent.futures
+import asyncio
+import nest_asyncio
+
 import numpy as np
 from dipy.core.gradients import gradient_table
+
+nest_asyncio.apply()
 
 
 class ModelFactory:
@@ -97,56 +103,138 @@ class TrivialB0Model:
 class TensorModel:
     """A wrapper of :obj:`dipy.reconst.dti.TensorModel."""
 
-    __slots__ = ("_model", "_S0", "_mask")
+    __slots__ = (
+        "_S0",
+        "_mask",
+        "_n_threads",
+        "_S0_chunks",
+        "_mask_chunks",
+        "_model_chunks"
+    )
 
     def __init__(self, gtab, S0=None, mask=None, **kwargs):
         """Instantiate the wrapped tensor model."""
-        from dipy.reconst.dti import TensorModel as DipyTensorModel
+        import dipy.reconst.dti as DipyTensorModel
+
+        for k, v in kwargs.items():
+            if k == 'n_threads':
+                self._n_threads = v
+            else:
+                self._n_threads = 1
 
         self._S0 = None
+        self._S0_chunks = None
         if S0 is not None:
             self._S0 = np.clip(
-                S0.astype("float32") / S0.max(),
-                a_min=1e-5,
-                a_max=1.0,
+                    S0.astype("float32") / S0.max(),
+                    a_min=1e-5,
+                    a_max=1.0,
             )
-        self._mask = mask
+            self._S0_chunks = np.split(S0, self._n_threads, axis=2)
+
+        self._mask = None
+        self._mask_chunks = None
         if mask is None and S0 is not None:
             self._mask = self._S0 > np.percentile(self._S0, 35)
+            self._mask_chunks = np.split(self._mask, self._n_threads, axis=2)
 
         if self._mask is not None:
             self._S0 = self._S0[self._mask.astype(bool)]
+            self._S0_chunks = np.split(self._S0, self._n_threads, axis=2)
 
         kwargs = {
-            k: v
-            for k, v in kwargs.items()
-            if k
-            in (
-                "min_signal",
-                "return_S0_hat",
-                "fit_method",
-                "weighting",
-                "sigma",
-                "jac",
-            )
+                k: v
+                for k, v in kwargs.items()
+                if k
+                   in (
+                           "min_signal",
+                           "return_S0_hat",
+                           "fit_method",
+                           "weighting",
+                           "sigma",
+                           "jac",
+                   )
         }
-        self._model = DipyTensorModel(gtab, **kwargs)
+
+        # Create a TensorModel for each chunk
+        self._model_chunks = [
+            DipyTensorModel(gtab, **kwargs)
+            for _ in range(self._n_threads)
+        ]
+
+    @staticmethod
+    def fit_chunk(model_chunk, data_chunk):
+        """Call model's fit."""
+        return model_chunk.fit(data_chunk)
 
     def fit(self, data, **kwargs):
-        """Call model's fit."""
+        """Fit the model chunk-by-chunk asynchronously."""
         if self._mask is not None:
             data = data[self._mask, ...]
-        self._model = self._model.fit(data)
+
+        # Split data into chunks of group of slices (axis=2)
+        data_chunks = np.split(data, self._n_threads, axis=2)
+
+        # Create a limited thread pool.
+        executor = concurrent.futures.ThreadPoolExecutor(
+            max_workers=self._n_threads,
+        )
+
+        loop = asyncio.get_event_loop()
+        fit_tasks = [
+            loop.run_in_executor(
+                executor,
+                self.fit_chunk,
+                self._model_chunks[i],
+                data_chunks[i]
+            )
+            for i in range(self._n_threads)
+        ]
+
+        try:
+            event_loop = asyncio.get_event_loop()
+            self._model_chunks = event_loop.run_until_complete(asyncio.gather(*fit_tasks))
+        finally:
+            pass
+
+    @staticmethod
+    def predict_chunk(model_chunk, S0_chunk, gradient, step=None):
+        """Call predict for chunk and return the predicted diffusion signal."""
+        return model_chunk.predict(
+                _rasb2dipy(gradient),
+                S0=S0_chunk,
+                step=step,
+        )
 
     def predict(self, gradient, step=None, **kwargs):
-        """Propagate model parameters and call predict."""
-        predicted = np.squeeze(
-            self._model.predict(
-                _rasb2dipy(gradient),
-                S0=self._S0,
-                step=step,
-            )
+        """Predict asynchronously chunk-by-chunk the diffusion signal."""
+        # Create a limited thread pool.
+        executor = concurrent.futures.ThreadPoolExecutor(
+            max_workers=self._n_threads,
         )
+
+        loop = asyncio.get_event_loop()
+        predict_tasks = [
+            loop.run_in_executor(
+                executor,
+                self.predict_chunk,
+                self._model_chunks[i],
+                self._S0_chunks[i],
+                gradient, step
+            )
+            for i in range(self._n_threads)
+        ]
+
+        try:
+            event_loop = asyncio.get_event_loop()
+            predicted = event_loop.run_until_complete(
+                asyncio.gather(*predict_tasks)
+            )
+        finally:
+            pass
+
+        predicted = np.squeeze(np.concatenate(predicted, axis=2))
+
         if predicted.ndim == 3:
             return predicted
 
